3d8afdafb2b034653862d08b0e56ed66f2ad06aa
Daniel Quinlan <daniel@chaosengine.net>
2015-01-15 18:45:58 -0800

    update print_pulses, add cmdline flags

--- print_pulses.py
+++ print_pulses.py
@@ -5,28 +5,49 @@
   
 import RPi.GPIO as GPIO  
 import time
+from optparse import OptionParser
 
-WALLBOX=13
-PULSE_WIDTH=50
+parser = OptionParser()
+parser.add_option("-p", "--pin", dest="pin",
+                  default=13,
+                  help="pin number based on Board numbering [%default]")
+parser.add_option("-b", "--bouncetime", dest="bouncetime",
+                  default="10",
+                  help="bounce time in ms. 0 to disable sw debounce [%default]")
+parser.add_option("-e", "--edgedetect", dest="edgedetect",
+                  default="rising",
+                  help="r[ising]|f[alling]|b[oth] [%default]")
+
+(options, args) = parser.parse_args(args=argv)
+
+if options.edgedetect in ["r", "rising"]:
+    EDGE_DETECT=GPIO.RISING
+elif option.edgedetect in ["f", "falling"]:
+    EDGE_DETECT=GPIO.FALLING
+elif option.edgedetect in ["b", "both"]:
+    EDGE_DETECT=GPIO.BOTH
 
 GPIO.setmode(GPIO.BOARD)  
-# pin 13 will go to 3.3V when a pulse is sent
-GPIO.setup(WALLBOX, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+GPIO.setup(options.pin, GPIO.IN)
 
 # now we'll define the threaded callback function  
 # this will run in another thread when our event is detected  
 def my_callback(channel):  
-    print "Rising edge detected on port %d at %s" % (channel, time.time())
+    print "edge detected on port %d at %s" % (channel, time.time())
   
 # The GPIO.add_event_detect() line below set things up so that  
 # when a rising edge is detected on port 13, regardless of whatever   
 # else is happening in the program, the function "my_callback" will be run  
 # It will happen even while the program is waiting for  
 # a falling edge on the other button.  
-GPIO.add_event_detect(WALLBOX, GPIO.RISING, callback=my_callback, bouncetime=PULSE_WIDTH)  
+if options.pulse_width > 0:
+    GPIO.add_event_detect(options.pin, EDGE_DETECT, callback=my_callback,
+            bouncetime=options.bouncetime)  
+else:
+    GPIO.add_event_detect(options.pin, EDGE_DETECT, callback=my_callback)
   
 try:  
-    print "Waiting for rising edge on port %d\n" % WALLBOX
+    print "Waiting for edge on port %d\n" % options.pin
     print "Press ^C to exit\n"
     while True:
         time.sleep(0.01)
